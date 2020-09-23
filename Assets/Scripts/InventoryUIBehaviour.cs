250554701c78cbb34361185bfa6b8127e89f4e6e
MaxSimon95 <maximilian.simon@ymail.com>
2020-09-23 19:58:57 +0200

    UI
    inventory scrolling up/down, Transfer amount selection, Sound

---
+++ Assets/Scripts/InventoryUIBehaviour.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+
+public class InventoryUIBehaviour : MonoBehaviour
+{
+    public bool isScrolledUp = false;
+    public AudioClip clickSound;
+    private AudioSource source;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("i"))
+        {
+            ChangeInventoryScroll();
+        }
+
+        if (Input.GetMouseButton(0) && isScrolledUp)
+        { 
+
+            if (
+                !RectTransformUtility.RectangleContainsScreenPoint
+                (
+                GameObject.Find("PanelInventory").GetComponent<RectTransform>(),
+                Input.mousePosition
+                )
+               )
+                {
+                ChangeInventoryScroll();
+            }
+        }
+    }
+
+   /* public void OnDeselect(BaseEventData eventData)
+    {
+        if (isScrolledUp)
+        {
+            ChangeInventoryScroll();
+        }
+        
+    } */
+
+    public void ChangeInventoryScroll()
+    {
+        source = GetComponent<AudioSource>();
+            source.PlayOneShot(clickSound, 1f);
+
+        // Inventory is fully visible
+        if (isScrolledUp)
+        {
+            Debug.Log("IS SCROLLED UP --> SCROLLING DOWN");
+            isScrolledUp = false;
+            LeanTween.moveY(GameObject.Find("PanelInventory").GetComponent<RectTransform>(), -307.8795f, 0.1f);
+
+            LeanTween.rotate(GameObject.Find("ButtonArrowInventoryImage").GetComponent<RectTransform>(), 180.0f, 0.1f);
+        }
+
+        // Only first Inventory line is visible
+        else {
+            Debug.Log("IS NOT SCROLLED UP --> SCROLLING UP");
+            isScrolledUp = true;
+            LeanTween.moveY(GameObject.Find("PanelInventory").GetComponent<RectTransform>(), 231f, 0.1f);
+            LeanTween.rotate(GameObject.Find("ButtonArrowInventoryImage").GetComponent<RectTransform>(), 180.0f, 0.1f);
+        }
+    }
+}
---
+++ Assets/Scripts/TransferAmountHandling.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+//using System.Diagnostics;
+using UnityEngine;
+
+public class TransferAmountHandling : MonoBehaviour
+{
+    public int currentTransferAmount = 10;
+    public AudioClip clickSound;
+    private AudioSource source;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        AdjustLabelTransferAmount();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Equals))
+        {
+            RaiseIngredientTransferAmount();
+        }
+
+        if (Input.GetKeyDown("-"))
+        {
+            LowerIngredientTransferAmount();
+        }
+    }
+
+    public void RaiseIngredientTransferAmount()
+    {
+        switch (currentTransferAmount)
+        {
+            case 1:
+                currentTransferAmount = 10;
+                break;
+            case 10:
+                currentTransferAmount = 100;
+                break;
+            case 100:
+                currentTransferAmount = 100;
+                break;
+        }
+        AdjustLabelTransferAmount();
+        source = GetComponent<AudioSource>();
+        source.PlayOneShot(clickSound, 1f);
+    }
+
+    public void LowerIngredientTransferAmount()
+    {
+        switch (currentTransferAmount)
+        {
+            case 1:
+                currentTransferAmount = 1;
+                break;
+            case 10:
+                currentTransferAmount = 1;
+                break;
+            case 100:
+                currentTransferAmount = 10;
+                break;
+        }
+        AdjustLabelTransferAmount();
+        source = GetComponent<AudioSource>();
+        source.PlayOneShot(clickSound, 1f);
+    }
+
+    private void AdjustLabelTransferAmount()
+    {
+        GameObject.Find("LabelTransferAmount").GetComponent<UnityEngine.UI.Text>().text = currentTransferAmount.ToString();
+        Debug.Log("TransferAmount set to " + currentTransferAmount);
+    }
+    
+}
