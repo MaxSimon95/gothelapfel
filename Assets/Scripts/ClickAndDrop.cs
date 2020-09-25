2f2413b69feaa55142a9b575b2044a4539fc12aa
MaxSimon95 <maximilian.simon@ymail.com>
2020-09-25 14:39:23 +0200

    click and drop

--- Assets/Scripts/ClickAndDrop.cs
+++ Assets/Scripts/ClickAndDrop.cs
@@ -1,18 +1,25 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+//using System.Diagnostics;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
-public class ClickAndDrop : MonoBehaviour
+public class ClickAndDrop : MonoBehaviour, IPointerDownHandler
 {
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("Click Down");
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        Debug.Log("Click Down");
     }
 }
