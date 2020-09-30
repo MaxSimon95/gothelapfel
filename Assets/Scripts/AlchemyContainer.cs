187da2331c8a9738a4ac7a78af0dafaf2181ddc6
MaxSimon95 <maximilian.simon@ymail.com>
2020-09-30 15:28:53 +0200

    ka

--- Assets/Scripts/AlchemyContainer.cs
+++ Assets/Scripts/AlchemyContainer.cs
@@ -4,10 +4,17 @@ using UnityEngine;
 
 public class AlchemyContainer : MonoBehaviour
 {
+
+    public List<GameObject> ingredients = new List<GameObject>();
+
+    
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        // Add parts to the list.
+        //ingredients.Add(new Part() { PartName = "crank arm", PartId = 1234 });
+        //ingredients.Add(new Part() { PartName = "chain ring", PartId = 1334 });
     }
 
     // Update is called once per frame
