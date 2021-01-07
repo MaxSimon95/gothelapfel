c24c3f44764c74a91b6f2d554e79a237a8fd8729
MaxSimon95 <maximilian.simon@ymail.com>
2021-01-08 00:22:18 +0100

    time variables and settings

---
+++ Assets/Scripts/GameLogic/GameTime.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameTime : MonoBehaviour
+{
+    public int yearSinceStart;
+    public enum Season
+    {
+        spring,
+        summer,
+        autumn,
+        winter
+    }
+    public int daysSinceStart;
+    public int daysSinceYearStart;
+    public int SecondinhosSinceStart;
+    public int hourOfTheDay;
+
+    public int yearLengthInDays;
+    public int dayLengthInHours;
+    public float hourLengthInSecondinhos;
+    public float secondinhoLengthInSeconds;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
