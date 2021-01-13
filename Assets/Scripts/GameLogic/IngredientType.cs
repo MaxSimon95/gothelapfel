4864f795b9f7917e01273ac58c7bf55715d3c630
MaxSimon95 <maximilian.simon@ymail.com>
2021-01-14 00:42:08 +0100

    fun varaibles

--- Assets/Scripts/GameLogic/IngredientType.cs
+++ Assets/Scripts/GameLogic/IngredientType.cs
@@ -10,6 +10,51 @@ public class IngredientType : MonoBehaviour
     public Sprite inventorySprite;
     public AudioClip ingredientSound;
     public bool burnsToAsh;
+
+    public float meltingTemperature;
+    public float boilingTemperature;
+
+    // 0 = no change, +100 = lethal dosis; -100 = enough to reverse lethal dosis
+    public int acidity;  
+    public int healthBrain;
+    public int healthSkin;
+    public int healthHeart;
+    public int healthRespiration;
+    public int healthEyes;
+    public int healthStomach;
+    public int healthInfection;
+
+    public float durationTempEffects; // duration in hours
+
+    // -100 removes effect completely, 0 doesn't change effect, +100 increases to maximum
+    public int tempHallucinations;
+    public int tempDizzyness;
+    public int tempRelaxation;
+    public int tempSensitivity;
+
+    // -100 to +100, 0 doesn't change.
+    public int tempChattiness;
+    public int tempIntelligence;
+    public int tempStrength;
+    public int tempNervosity;
+    public int tempAgreeableness;
+
+
+
+
+    // 0 = tasteless, -100 = YUCK, +100 tasty
+    public int taste;
+
+    // 0 = nothing, 100 = daylight
+    public int luminosity;
+
+    
+
+
+
+    
+
+
   
     void Start()
     {
