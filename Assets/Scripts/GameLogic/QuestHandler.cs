6644493a3a3161daaf670a686b67553ba6add82e
MaxSimon95 <maximilian.simon@ymail.com>
2021-01-15 18:33:25 +0100

    small stuff

---
+++ Assets/Scripts/GameLogic/QuestHandler.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuestHandler : MonoBehaviour
+{
+    public int payment;
+
+    public IngredientType requestedIngredientType;
+
+    public int requestedAmount;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
