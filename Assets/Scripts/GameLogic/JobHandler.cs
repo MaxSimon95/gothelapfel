6009312e9a804aa20527c9cd0555eca4cde72b07
msimon <msimon@silversurfer7.de>
2021-01-16 23:49:24 +0100

    job progress

---
+++ Assets/Scripts/GameLogic/JobHandler.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JobHandler : MonoBehaviour
+{
+    public int payment;
+
+    public IngredientType requestedIngredientType;
+
+    public int requestedAmount;
+
+    public int startDays;
+    public int remainingDays;
+    public bool jobIsSaved;
+    public int orderNumber;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        remainingDays = startDays;
+        orderNumber = calculateOrderNumber();
+    }
+
+    int calculateOrderNumber()
+    {
+        int tempNumber = 0;
+        if (!jobIsSaved)
+        {
+            tempNumber += 999999;
+        }
+
+        tempNumber += remainingDays;
+
+        return tempNumber;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/GameLogic/JobsManagement.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JobsManagement : MonoBehaviour
+{
+
+    public Transform panelFeaturedJobs;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void UpdateJobs()
+    {
+        
+    }
+
+    void OrderJobs()
+    {
+        //MyGameObjectList = MyGameObjectList.OrderBy(e => e.GetComponent<MyComponent>().MyComponentMember).ToList();
+    }
+
+    void UpdateFeaturedJobs()
+    {
+
+    }
+
+}
