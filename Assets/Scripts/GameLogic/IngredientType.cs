5bd0739114ce0017a8e2523caba98f2bd0d5bf05
MaxSimon95 <maximilian.simon@ymail.com>
2021-01-18 00:51:43 +0100

    notebook ui

--- Assets/Scripts/GameLogic/IngredientType.cs
+++ Assets/Scripts/GameLogic/IngredientType.cs
@@ -14,9 +14,11 @@ public class IngredientType : MonoBehaviour
     public float meltingTemperature;
     public float boilingTemperature;
 
+    // labels: <=-100 = lethal ___ poison, -99 to -50 strong ___ poison, -49 to 0 weak ___ poison, 0 to 49 light ___ medicine, 50 to 99 ___ potent medicine, 100+ extremely potent medicine
+
     // 0 = no change, +100 = lethal dosis; -100 = enough to reverse lethal dosis
     public int acidity;  
-    public int healthBrain;
+    public int healthBrain;  //
     public int healthSkin;
     public int healthHeart;
     public int healthRespiration;
@@ -29,21 +31,26 @@ public class IngredientType : MonoBehaviour
 
     // -100 removes effect completely, 0 doesn't change effect, +100 increases to maximum
     public int tempHallucinations;
-    public int tempDizzyness;
-    public int tempRelaxation;
-    public int tempSensitivity;
+    public int tempDizzyness;   
+    public int tempRelaxation;  // opposite: tension
+    public int tempSensitivity; // opposite: numbness
+    public int tempTieredness;
+
+    // 1 to 33 slightly amplify/reduce ___, 34 to 66 moderately amplify/reduce ____, 67 to 100 greatly amplify/reduce
 
     // -100 to +100, 0 doesn't change.
     public int tempChattiness;
-    public int tempIntelligence;
-    public int tempStrength;
-    public int tempNervosity;
+    public int tempIntelligence;  
+    public int tempStrength; 
+    //public int tempNervosity;
     public int tempAgreeableness;
     public int tempLust;
+    public int tempFear; // opposite: Bravery
+    
 
 
 
-
+    // labels = <=-100 to -50 disgusting taste, -50 to 0 unappetizing taste, 0 tasteless, 0 to 50 agreeable taste, 50 to 100 delicious taste
     // 0 = tasteless, -100 = YUCK, +100 tasty
     public int taste;
 
---
+++ Assets/Scripts/UI/NotebookJobs.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NotebookJobs : MonoBehaviour
+{
+    // Start is called before the first frame update
+    
+    
+    void Start()
+    {
+        transform.GetChild(0).localScale = new Vector3(0, 0, 0);
+    }
+
+    public void OpenNotebook()
+    {
+        transform.GetChild(0).localScale = new Vector3(1, 1, 1);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
