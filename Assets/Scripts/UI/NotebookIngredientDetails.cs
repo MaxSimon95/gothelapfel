76ac0a16f2dacfc288404c86e2bd156fff863c2b
MaxSimon95 <maximilian.simon@ymail.com>
2021-02-08 23:21:53 +0100

    detail ingredients data

--- Assets/Scripts/UI/NotebookIngredientDetails.cs
+++ Assets/Scripts/UI/NotebookIngredientDetails.cs
@@ -7,10 +7,25 @@ public class NotebookIngredientDetails : MonoBehaviour
 
     public IngredientType ingredient;
 
+    public GameObject UIname;
+    public GameObject UIimage_withDescription;
+    public GameObject UIimage_noDescription;
+    public GameObject UIdescription;
+
+    public Transform effectPanelsParent;
+
+    private List<Transform> effectPanels = new List<Transform>();
+    private List<IngredientEffect> effects = new List<IngredientEffect>();
+    private List<int> effectIntensities = new List<int>();
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        foreach(Transform child in effectPanelsParent)
+        {
+            effectPanels.Add(child);
+        }
     }
 
     // Update is called once per frame
@@ -29,6 +44,50 @@ public class NotebookIngredientDetails : MonoBehaviour
 
     public void UpdateIngredientDetails()
     {
+        effects = ingredient.effects;
+        effectIntensities = ingredient.effectIntensities;
+
+        UIname.GetComponent<UnityEngine.UI.Text>().text = ingredient.ingredientName;
+        
+        if(ingredient.description == "")
+        {
+            UIdescription.transform.localScale = new Vector3(0, 0, 0);
+            UIimage_noDescription.GetComponent<UnityEngine.UI.Image>().sprite = ingredient.inventorySprite;
+            UIimage_withDescription.transform.localScale = new Vector3(0, 0, 0);
+            UIimage_noDescription.transform.localScale = new Vector3(1, 1, 1);
+        }
+        else
+        {
+            
+            UIimage_withDescription.GetComponent<UnityEngine.UI.Image>().sprite = ingredient.inventorySprite;
+            UIimage_withDescription.transform.localScale = new Vector3(1, 1, 1);
+            UIimage_noDescription.transform.localScale = new Vector3(0, 0, 0);
+            UIdescription.transform.localScale = new Vector3(1, 1, 1);
+            UIdescription.GetComponent<UnityEngine.UI.Text>().text = ingredient.description;
+        }
+
+        for (int i = 0; i < effectPanels.Count; i++)
+        {
+            effectPanels[i].localScale = new Vector3(0, 0, 0);
+
+            //Debug.Log(i);
+
+            //Debug.Log(JobsManagement.activeJobList.Count);
+            if (i < effects.Count)
+            {
+                effectPanels[i].localScale = new Vector3(1, 1, 1);
+                //Debug.Log(i + "set to visible, local scale: " + effectPanels[i].localScale);
+                //Debug.Log(i);
+                // CAREFUL: THIS STUFF IS ORDERING SENSITIVE. YOU MESS WITH THE ORDERING, YOU MESS WITH THE CONTENTS, YO! 
+                //Debug.Log(i);
+
+                effectPanels[i].GetChild(1).gameObject.GetComponent<UnityEngine.UI.Text>().text = effects[i].effectName;
+
+
+                Debug.Log(i + "info updated");
+
+            }
+        }
 
     }
 }
