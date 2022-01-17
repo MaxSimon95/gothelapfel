ad8b15d61f978077ea26b27d00d063af66d721f2
MaxSimon95 <maximilian.simon@ymail.com>
2022-01-17 22:25:05 +0100

    player character sprite

--- Assets/Scripts/Scene/PlayerCharacter.cs
+++ Assets/Scripts/Scene/PlayerCharacter.cs
@@ -6,6 +6,10 @@ public class PlayerCharacter : MonoBehaviour
 {
     public float movementSpeed;
     public GameObject currentRoom;
+    public Sprite[] sprites;
+
+    enum Direction { SW, S, SE, E, NE, N, NW, W};
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,7 +17,64 @@ public class PlayerCharacter : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
+    {
+
+        // vertical and horizontal movement
+
+        if ((Input.GetKey("w")) && !((Input.GetKey("a")|| Input.GetKey("s"))|| Input.GetKey("d")))
+        {
+            CharacterMovement(Direction.N, 0, movementSpeed * 1.0f);
+           
+        }
+
+        if ((Input.GetKey("s")) && !((Input.GetKey("w") || Input.GetKey("a")) || Input.GetKey("d")))
+        {
+
+            CharacterMovement(Direction.S, 0, movementSpeed * -1.0f);
+        }
+
+        if ((Input.GetKey("a")) && !((Input.GetKey("w") || Input.GetKey("s")) || Input.GetKey("d")))
+        {
+            CharacterMovement(Direction.W, movementSpeed * -1.0f,0);
+
+        }
+
+        if ((Input.GetKey("d")) && !((Input.GetKey("w") || Input.GetKey("a")) || Input.GetKey("s")))
+        {
+
+            CharacterMovement(Direction.E, movementSpeed * 1.0f, 0);
+        }
+
+        // isometric movement
+
+        if ((Input.GetKey("w") && Input.GetKey("a")) && !( Input.GetKey("s") || Input.GetKey("d")))
+        {
+            CharacterMovement(Direction.NW, movementSpeed * -1.0f, movementSpeed * 0.47f);
+        }
+
+        if ((Input.GetKey("w") && Input.GetKey("d")) && !(Input.GetKey("s") || Input.GetKey("a")))
+        {
+            CharacterMovement(Direction.NE, movementSpeed * 1.0f, movementSpeed * 0.47f);
+        }
+
+        if ((Input.GetKey("s") && Input.GetKey("a")) && !(Input.GetKey("w") || Input.GetKey("d")))
+        {
+            CharacterMovement(Direction.SW, movementSpeed * -1.0f, movementSpeed * -0.47f);
+        }
+
+        if ((Input.GetKey("s") && Input.GetKey("d")) && !(Input.GetKey("w") || Input.GetKey("a")))
+        {
+            CharacterMovement(Direction.SE, movementSpeed * 1.0f, movementSpeed * -0.47f);
+        }
+
+        GetComponent<RenderOrderAdjustment>().AdjustRenderOrder();
+
+    }
+
+    /*
+     * 
+     void Update()
     {
 
         // vertical and horizontal movement
@@ -76,4 +137,45 @@ public class PlayerCharacter : MonoBehaviour
         GetComponent<RenderOrderAdjustment>().AdjustRenderOrder();
 
     }
+     */
+
+    void CharacterMovement(Direction direction, float distanceX, float distanceY)
+    {
+        switch(direction)
+        {
+            case Direction.SW:
+
+                GetComponent<SpriteRenderer>().sprite = sprites[0];
+                break;
+            case Direction.S:
+                GetComponent<SpriteRenderer>().sprite = sprites[1];
+                break;
+            case Direction.SE:
+                GetComponent<SpriteRenderer>().sprite = sprites[2];
+                break;
+            case Direction.E:
+                GetComponent<SpriteRenderer>().sprite = sprites[3];
+                break;
+            case Direction.NE:
+                GetComponent<SpriteRenderer>().sprite = sprites[4];
+                break;
+            case Direction.N:
+                GetComponent<SpriteRenderer>().sprite = sprites[5];
+                break;
+            case Direction.NW:
+                GetComponent<SpriteRenderer>().sprite = sprites[6];
+                break;
+            case Direction.W:
+                GetComponent<SpriteRenderer>().sprite = sprites[7];
+                break;
+        }
+
+        this.gameObject.transform.position = this.gameObject.transform.position + new Vector3(distanceX, distanceY, 0);
+    }
+
+    void UpdateSpriteDirection()
+    {
+
+    }
+
 }
