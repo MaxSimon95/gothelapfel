7b39a248975baaba650c0d806bed0645ff2667ab
MaxSimon95 <maximilian.simon@ymail.com>
2023-01-08 21:25:21 +0100

    ui frontdoor elements

--- Assets/Scripts/GameLogic/RegionHandler.cs
+++ Assets/Scripts/GameLogic/RegionHandler.cs
@@ -5,6 +5,37 @@ using UnityEngine;
 public class RegionHandler : MonoBehaviour
 {
     // Start is called before the first frame update
+    public string regionName;
+    public float distanceToHome;
+    public bool currentlyAccessible;
+
+    public List<IngredientType> ingredientTypesSpring;
+    public List<IngredientType> ingredientTypesSummer;
+    public List<IngredientType> ingredientTypesAutumn;
+    public List<IngredientType> ingredientTypesWinter;
+
+    public List<int> amountsMinSpring;
+    public List<int> amountsMinSummer;
+    public List<int> amountsMinAutumn;
+    public List<int> amountsMinWinter;
+
+    public List<int> amountsMaxSpring;
+    public List<int> amountsMaxSummer;
+    public List<int> amountsMaxAutumn;
+    public List<int> amountsMaxWinter;
+
+    public List<int> randomSearchChanceSpring;
+    public List<int> randomSearchChanceSummer;
+    public List<int> randomSearchChanceAutumn;
+    public List<int> randomSearchChanceWinter;
+
+    public List<int> specificSearchChanceSpring;
+    public List<int> specificSearchChanceSummer;
+    public List<int> specificSearchChanceAutumn;
+    public List<int> specificSearchChanceWinter;
+
+
+
     void Start()
     {
         
---
+++ Assets/Scripts/UI/TripPlanningHandler.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TripPlanningHandler : MonoBehaviour
+{
+    public GameObject regions;
+    public GameObject UIDropdownRegion;
+    public GameObject UIDropdownActivity;
+    public GameObject UIDropdownIngredient;
+    public GameObject UIButtonCancel;
+    public GameObject UIButtonStartTrip;
+    public GameObject UIRadioAllAvailableIngredients;
+    public GameObject UIRadioSpecificIngredient;
+    public GameObject UIDropdownTime;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void DropdownRegionChanged(int dropdownIndex)
+    {
+
+    }
+
+    void DropdownActivityChanged(int dropdownIndex)
+    {
+
+    }
+
+    void DropdownIngredientChanged(int dropdownIndex)
+    {
+
+    }
+
+    void DropdownTimeChanged(int dropdownIndex)
+    {
+
+    }
+
+    void RadioAllAvailableIngredientsSelected()
+    {
+
+    }
+
+    void RadioSpecificIngredientSelected()
+    {
+
+    }
+
+    void StartTrip()
+    {
+
+    }
+}
